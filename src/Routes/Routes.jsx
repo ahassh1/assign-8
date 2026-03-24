137e55cf9d4203982738a69467dd3d09cff00df4
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-03-24 15:01:01 +0600

    router set up done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root";
+import Home from "../pages/Home";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
+  },
+]);
